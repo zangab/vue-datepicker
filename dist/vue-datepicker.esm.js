3d71f13decea2ebb24bbfcb63e6bcde91bbc5f01
gabo <zet.zangi@gmail.com>
2023-11-03 00:20:59 +0100

    feat: remove unused locales

--- dist/vue-datepicker.esm.js
+++ dist/vue-datepicker.esm.js
@@ -118,83 +118,6 @@ var localeObject$3 = {
   rangeHeaderText: 'Vom %d Bis %d'
 };
 
-var localeObject$4 = {
-  name: 'he',
-  weekdays: 'ראשון_שני_שלישי_רביעי_חמישי_שישי_שבת'.split('_'),
-  weekdaysShort: 'א׳_ב׳_ג׳_ד׳_ה׳_ו׳_ש׳'.split('_'),
-  weekdaysMin: 'א׳_ב׳_ג׳_ד׳_ה׳_ו_ש׳'.split('_'),
-  months: 'ינואר_פברואר_מרץ_אפריל_מאי_יוני_יולי_אוגוסט_ספטמבר_אוקטובר_נובמבר_דצמבר'.split('_'),
-  monthsShort: 'ינו_פבר_מרץ_אפר_מאי_יונ_יול_אוג_ספט_אוק_נוב_דצמ'.split('_'),
-  ordinal: function ordinal(n) {
-    return n;
-  },
-  formats: {
-    LT: 'HH:mm',
-    LTS: 'HH:mm:ss',
-    L: 'DD/MM/YYYY',
-    LL: 'D [ב]MMMM YYYY',
-    LLL: 'D [ב]MMMM YYYY HH:mm',
-    LLLL: 'dddd, D [ב]MMMM YYYY HH:mm',
-    l: 'D/M/YYYY',
-    ll: 'D MMM YYYY',
-    lll: 'D MMM YYYY HH:mm',
-    llll: 'ddd, D MMM YYYY HH:mm'
-  },
-  buttonValidate: 'אישור',
-  buttonCancel: 'ביטול',
-  rangeHeaderText: 'מתאריך %d עד תאריך %d'
-};
-
-var localeObject$5 = {
-  name: 'ru',
-  weekdays: 'Воскресенье_Понедельник_Вторник_Среда_Четверг_Пятница_Суббота'.split('_'),
-  weekdaysShort: 'Вс_Пн_Вт_Ср_Чт_Пт_Сб'.split('_'),
-  weekdaysMin: 'Вс_Пн_Вт_Ср_Чт_Пт_Сб'.split('_'),
-  months: 'Январь_Февраль_Март_Апрель_Май_Июнь_Июль_Август_Сентябрь_Октябрь_Ноябрь_Декабрь'.split('_'),
-  monthsShort: 'Янв_Фев_Мар_Апр_Май_Июн_Июл_Авг_Сен_Окт_Ноя_Дек'.split('_'),
-  weekStart: 1,
-  formats: {
-    LT: 'HH:mm',
-    LTS: 'HH:mm:ss',
-    L: 'DD.MM.YYYY',
-    LL: 'D MMMM YYYY',
-    LLL: 'D MMMM YYYY HH:mm',
-    LLLL: 'dddd, D MMMM YYYY HH:mm'
-  },
-  ordinal: function ordinal(n) {
-    return n;
-  },
-  buttonValidate: 'Ок',
-  buttonCancel: 'Отмена',
-  rangeHeaderText: 'От %d До %d'
-};
-
-var localeObject$6 = {
-  name: 'sv',
-  weekdays: 'Söndag_Måndag_Tisdag_Onsdag_Torsdag_Fredag_Lördag'.split('_'),
-  weekdaysShort: 'Sön_Mån_Tis_Ons_Tor_Fre_Lör'.split('_'),
-  weekdaysMin: 'Sö_Må_Ti_On_To_Fr_Lö'.split('_'),
-  months: 'januari_februari_mars_april_maj_juni_juli_augusti_september_oktober_november_december'.split('_'),
-  monthsShort: 'jan_feb_mar_apr_maj_jun_jul_aug_sep_okt_nov_dec'.split('_'),
-  weekStart: 0,
-  formats: {
-    LT: 'HH:mm',
-    LTS: 'HH:mm:ss',
-    L: 'YYYY-MM-DD',
-    LL: 'D MMMM YYYY',
-    LLL: 'D MMMM YYYY HH:mm',
-    LLLL: 'dddd, D MMMM YYYY HH:mm'
-  },
-  ordinal: function ordinal(n) {
-    var s = ['th', 'st', 'nd', 'rd'];
-    var v = n % 100;
-    return "[".concat(n).concat(s[(v - 20) % 10] || s[v] || s[0], "]");
-  },
-  buttonValidate: 'Ok',
-  buttonCancel: 'Avbryt',
-  rangeHeaderText: 'Från %d till %d'
-};
-
 
 
 var locales = /*#__PURE__*/Object.freeze({
@@ -202,10 +125,7 @@ var locales = /*#__PURE__*/Object.freeze({
   fr: localeObject$1,
   en: localeObject,
   es: localeObject$2,
-  de: localeObject$3,
-  he: localeObject$4,
-  ru: localeObject$5,
-  sv: localeObject$6
+  de: localeObject$3
 });
 
 var Z_INDEX_LIST = {
--- src/locale/index.js
+++ src/locale/index.js
@@ -2,16 +2,10 @@ import en from './translations/en';
 import fr from './translations/fr';
 import es from './translations/es';
 import de from './translations/de';
-import he from './translations/he';
-import ru from './translations/ru';
-import sv from './translations/sv';
 
 export {
   fr,
   en,
   es,
-  de,
-  he,
-  ru,
-  sv,
+  de
 };
